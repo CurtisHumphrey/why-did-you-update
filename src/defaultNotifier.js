e9702f49c1ec1a09675e7b4c3d7850cf7ab3262a
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2016-08-17 19:18:38 -0500

    It is helpful we reducing the redraws to have a total count

--- src/defaultNotifier.js
+++ src/defaultNotifier.js
@@ -1,6 +1,8 @@
 const FUNC_WARNING = `Value is a function. Possibly avoidable re-render?`
 const AVOIDABLE_WARNING = `Value did not change. Avoidable re-render!`
 
+let warning_count = 1
+
 export const defaultNotifier = ({name, prev, next, type}) => {
   console.group(name)
 
@@ -9,7 +11,8 @@ export const defaultNotifier = ({name, prev, next, type}) => {
   } else {
     console.warn(FUNC_WARNING)
   }
-
+  
+  console.log('count', warning_count++)
   console.log(`%cbefore`, `font-weight: bold`, prev)
   console.log(`%cafter `, `font-weight: bold`, next)
   console.groupEnd()
